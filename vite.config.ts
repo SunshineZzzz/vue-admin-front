369ce0ba06eb0d811ce85ed6090133e6ef53aec3
sunshinezzz <605855436@qq.com>
2025-04-05 18:39:38 +0800

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -11,12 +10,12 @@ export default defineConfig({
     vueDevTools(),
   ],
   server: {
-	// 默认启动时的端口号
-	port: 8080,
-	// 自动打开默认浏览器
-	open: true,
-	// 允许跨域
-	cors: true,
+	  // 默认启动时的端口号 
+	  port: 8080,
+	  // 自动打开默认浏览器
+	  open: true,
+	  // 允许跨域
+	  cors: true,
   },
   resolve: {
     alias: {
